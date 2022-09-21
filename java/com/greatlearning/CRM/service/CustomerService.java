8170e5aacefda542230b449611da54693f68a330
Swethana01 <105113322+Swethana01@users.noreply.github.com>
2022-09-21 17:01:54 +0530

    Add files via upload

---
+++ java/com/greatlearning/CRM/service/CustomerService.java
@@ -0,0 +1,23 @@
+package com.greatlearning.CRM.service;
+
+import java.util.List;
+
+
+
+import com.greatlearning.CRM.entity.Customer;
+
+
+public interface CustomerService {
+
+
+	public Customer findById(int theId);
+
+	public void save(Customer thecustomer);
+
+	public void deleteById(int theId);
+
+	public List<Customer> findAll();
+
+	
+
+}
